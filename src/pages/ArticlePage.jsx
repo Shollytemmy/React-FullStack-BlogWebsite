4901d30549a6aa3722728dc0142480bb581d0cf2
Bunyameen <bunyameen@gmail.com>
2023-10-16 03:10:26 +0100

    Dynamic data rendered

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -1,12 +1,19 @@
+import React from "react"
 import { useParams } from "react-router-dom"
+import articles from "./article-content"
 
 const ArticlePage = () => {
   const {articleId} = useParams()
+  const article = articles.find((article) => article.name === articleId)
+  
   return (
-    <div>
-      <p>This is the Article Page with the article id: {articleId}</p>
+    <React.Fragment>
+      <h1>{article.title}</h1>
+
+      {article.content.map((paragraph) => <p key={paragraph}>{paragraph}</p>)}
       
-    </div>
+      
+    </React.Fragment>
   )
 }
 
